c31819c8d1e160e861f43504f2e353937a018ca1
aleksem07 <aleksem07@gmail.com>
2023-01-26 00:29:10 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const numToString = n.toString();
+    const toArray = numToString.split("");
+    const arrReverse = toArray.reverse();
+    const arrToString = arrReverse.join("");
+    const stringToNum = parseFloat(arrToString);
+    return stringToNum;
+};
